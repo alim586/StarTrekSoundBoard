84ecf0abd81f9134df2d5018699cf237ccdab2bc
innerspeakerz <rensjanvinke@gmail.com>
2018-02-04 17:59:53 +0100

    added warpcorebreach and scorpion and fox

--- app/src/main/java/random/projects/startreksoundboard/Home.java
+++ app/src/main/java/random/projects/startreksoundboard/Home.java
@@ -20,8 +20,14 @@ public class Home extends Activity {
         final Button engage = findViewById(R.id.engage);
         final Button borg1 = findViewById(R.id.borg1);
         final Button kill = findViewById(R.id.kill);
+        final Button scorpion = findViewById(R.id.scorpion);
+        final Button warpcorebreach = findViewById(R.id.warpcorebreach);
         final MediaPlayer mediaPlayerEngage = MediaPlayer.create(getApplicationContext(), R.raw.engage);
         final MediaPlayer mediaPlayerBorg1 = MediaPlayer.create(getApplicationContext(), R.raw.borg1);
+        final MediaPlayer mediaPlayerScorpion = MediaPlayer.create(getApplicationContext(), R.raw.scorpionnfox);
+        final MediaPlayer mediaPlayerWarpcorebreach = MediaPlayer.create(getApplicationContext(), R.raw.warpcorebreachsoonerthanyouthink_ep);
+
+
         engage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -36,6 +42,24 @@ public class Home extends Activity {
                     return;
                 }
                 mediaPlayerEngage.start();
+                System.out.println("duration track: " + duration);
+            }
+        });
+
+        scorpion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                int audioSessionId = mediaPlayerScorpion.getAudioSessionId();
+
+
+                System.out.println("sessionId" + audioSessionId);
+                int duration = mediaPlayerEngage.getDuration();
+
+                if (mediaPlayerScorpion.isPlaying()) {
+                    System.out.println("media player is playing");
+                    return;
+                }
+                mediaPlayerScorpion.start();
                 System.out.println("duration track: " + duration);
             }
         });
@@ -57,6 +81,24 @@ public class Home extends Activity {
                 System.out.println("duration track: " + duration);
             }
         });
+
+        warpcorebreach.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                int audioSessionId = mediaPlayerWarpcorebreach.getAudioSessionId();
+
+
+                System.out.println("sessionId" + audioSessionId);
+                int duration = mediaPlayerWarpcorebreach.getDuration();
+
+                if (mediaPlayerWarpcorebreach.isPlaying()) {
+                    System.out.println("media player is playing");
+                    return;
+                }
+                mediaPlayerWarpcorebreach.start();
+                System.out.println("duration track: " + duration);
+            }
+        });
         kill.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -67,6 +109,14 @@ public class Home extends Activity {
                 if (mediaPlayerBorg1.isPlaying()) {
                     mediaPlayerBorg1.pause();
                     mediaPlayerBorg1.seekTo(0);
+                }
+                if (mediaPlayerScorpion.isPlaying()) {
+                    mediaPlayerScorpion.pause();
+                    mediaPlayerScorpion.seekTo(0);
+                }
+                if (mediaPlayerWarpcorebreach.isPlaying()) {
+                    mediaPlayerWarpcorebreach.pause();
+                    mediaPlayerWarpcorebreach.seekTo(0);
                 }
             }
         });
